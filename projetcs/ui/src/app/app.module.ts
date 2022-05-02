e6aa99bdcb3285e7229783e555c3c0e87ba1b5b9
Andrey Kozel <andrey.kozel@spanning.com>
2022-05-02 14:54:50 +0400

    Added angular i18n

--- projetcs/ui/src/app/app.module.ts
+++ projetcs/ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './core/app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,8 @@ import { SplashComponent } from './pages/splash/splash.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpLoaderFactory } from "./core/translation.factory";
 
 @NgModule({
   declarations: [
@@ -22,10 +24,24 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'ru',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('ru');
+    translate.use('ru');
+  }
+
 }
