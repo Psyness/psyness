7853ff61ed3b50ec779708b198cd94458a0ae7b6
Andrey Kozel <andrey.kozel@spanning.com>
2022-05-15 16:02:02 +0400

    Improved clients page

--- projetcs/ui/src/app/pages/clients/clients.component.ts
+++ projetcs/ui/src/app/pages/clients/clients.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../services/user.service";
-import { User } from "../../models/user";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { CalendarData } from "../../models/appointment";
+import { MatDialog } from "@angular/material/dialog";
+import { AppointmentService } from "../../services/appointment.service";
 
 @Component({
   selector: 'app-clients',
@@ -10,25 +12,42 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 })
 export class ClientsComponent implements OnInit {
 
-  public displayedColumns: string[] = ['username', 'firstName', 'lastName']
-  public users: User[] = [];
+  public calendarData: CalendarData = { appointments: [], users: [], loading: false };
+  public viewDate = new Date()
+  public locale: string = 'ru';
 
   constructor(
+    private readonly dialog: MatDialog,
     private readonly snackBar: MatSnackBar,
+    private readonly appointmentService: AppointmentService,
     private readonly userService: UserService
   ) {
   }
 
   ngOnInit(): void {
+    this.reloadEvents();
     this.userService.findClients()
-      .subscribe(users => {
-        this.users = users
-      });
+      .subscribe(users => this.calendarData = { ...this.calendarData, users })
+  }
+
+  public setSelectedUserId(userId?: string) {
+    this.calendarData.attendeeId = userId;
+    this.reloadEvents();
+  }
+
+  public reloadEvents() {
+    this.calendarData = { ...this.calendarData, loading: true };
+    if (this.calendarData.attendeeId) {
+      this.appointmentService.getContractorAppointments(this.calendarData.attendeeId)
+        .subscribe(appointments => this.calendarData = { ...this.calendarData, appointments, loading: false });
+    } else {
+      this.appointmentService.getAppointments()
+        .subscribe(appointments => this.calendarData = { ...this.calendarData, appointments, loading: false });
+    }
   }
 
   createInvitation() {
     this.userService.createInvitation()
       .subscribe(invitationUrl => this.snackBar.open(invitationUrl, 'Close'))
   }
-
 }
--- projetcs/ui/src/app/pages/psychologists/psychologists.component.ts
+++ projetcs/ui/src/app/pages/psychologists/psychologists.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CalendarEvent } from 'angular-calendar';
 import { MatDialog } from "@angular/material/dialog";
 import { AppointmentService } from "../../services/appointment.service";
-import { AppointmentInfo, CalendarData } from "../../models/appointment";
-import { Observable } from "rxjs";
-import { SessionService } from "../../services/session.service";
-import { User } from "../../models/user";
+import { CalendarData } from "../../models/appointment";
 import { UserService } from "../../services/user.service";
 
 @Component({
@@ -16,23 +12,18 @@ import { UserService } from "../../services/user.service";
 })
 export class PsychologistsComponent implements OnInit {
 
-  public appointments$?: Observable<CalendarEvent<AppointmentInfo>[]>;
   public calendarData: CalendarData = { appointments: [], users: [], loading: false };
-  public user?: User;
   public viewDate = new Date()
   public locale: string = 'ru';
 
   constructor(
     private readonly dialog: MatDialog,
     private readonly appointmentService: AppointmentService,
-    private readonly sessionService: SessionService,
     private readonly userService: UserService
   ) {
   }
 
   ngOnInit(): void {
-    this.sessionService.getSession()
-      .subscribe(user => this.user = user)
     this.reloadEvents();
     this.userService.findPsychologists()
       .subscribe(users => this.calendarData = { ...this.calendarData, users })
